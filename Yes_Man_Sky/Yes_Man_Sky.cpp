387e020a2c54d6d0904bcd55f60bb65978250a40
EdytaJanicka <edyta.janicka0@gmail.com>
2021-01-17 13:21:06 +0100

    WordWrapping

--- Yes_Man_Sky/Yes_Man_Sky.cpp
+++ Yes_Man_Sky/Yes_Man_Sky.cpp
@@ -4,26 +4,83 @@
 #include <iostream>
 #include <stdlib.h>
 #include <windows.h>
+#include <sstream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 
-void writeSentenceSlow(string hello) {
+void WriteSentenceSlow(string hello) {
 
 	int x = 0;
 
 	while (hello[x] != '\0')
 	{
 		cout << hello[x];
-		Sleep(50);
+		Sleep(50);															// making console write letter by letter
 
-		if (hello[x] == '.' || hello[x] == '?') {
-			Sleep(300);
+		if (hello[x] == '.' || hello[x] == '?' || hello[x] == '!') {
+			Sleep(300);													// waiting at the end of the sentence
 		}
 		x++;
 	};
 
+}
+
+int GetBufferWidth()
+{
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	int bufferWidth, result;
+
+	result = GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo);
+
+	if (result)
+	{
+		bufferWidth = bufferInfo.dwSize.X;
+	}
+
+	return bufferWidth;
+}
+
+
+void OutputText(std::string s)
+{
+	int bufferWidth = GetBufferWidth();
+
+	for (unsigned int i = 1; i <= s.length(); i++)
+	{
+		char c = s[i - 1];
+
+		int spaceCount = 0;
+
+		// Add whitespace if newline detected.
+		if (c == '\n')
+		{
+			int charNumOnLine = ((i) % bufferWidth);
+			spaceCount = bufferWidth - charNumOnLine;
+			s.insert((i - 1), (spaceCount), ' ');			//insert space before newline break
+			i += (spaceCount);							//jump forward in string to character at beginning of next line
+			continue;
+		}
+
+		if ((i % bufferWidth) == 0)
+		{
+			if (c != ' ')
+			{
+				for (int j = (i - 1); j > -1; j--)
+				{
+					if (s[j] == ' ')
+					{
+						s.insert(j, spaceCount, ' ');
+						break;
+					}
+					else spaceCount++;
+				}
+			}
+		}
+	}
+
+	WriteSentenceSlow(s);
 }
 
 
@@ -40,17 +97,13 @@ int main()
 		while (!openprologue.eof())
 		{
 			getline(openprologue, prologue);
-			if (prologue.find("  ")) 
-			{
-				cout << endl;
-			};
-			writeSentenceSlow(prologue);
+			OutputText(prologue);
 		}
 	}
 	cout << "\n\n";
 	getline(cin, name);
 	string nametext = "\nSo, your name is " + name + ", huh? Okay, let's get to work, shall we?\n\n";
-	writeSentenceSlow(nametext);
+	OutputText(nametext);
 	openprologue.close();
 	return 0;
 }
